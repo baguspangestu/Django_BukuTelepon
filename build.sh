fb1804eb84415e96e88f9b597d9b49cc684cd57a
Bagus Pangestu <baguspangestu44@gmail.com>
2022-11-24 13:22:34 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 poetry install
-python manage.py collectstatic --no-input
-python manage.py migrate
 pip install --upgrade pip
 pip install --force-reinstall -U setuptools
+python manage.py collectstatic --no-input
+python manage.py migrate
